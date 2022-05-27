e1f1c0f3b5328c51867788f237f9b357d617a6cd
a5chin <capricorn42551230@gmail.com>
2022-05-28 03:23:58 +0900

    adapted format to examples

--- examples/pretrain_darknet.py
+++ examples/pretrain_darknet.py
@@ -1,10 +1,10 @@
 import argparse
 import pathlib
+import random
 import sys
 import warnings
 from typing import Dict
 
-import random
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
@@ -16,8 +16,8 @@ warnings.filterwarnings("ignore")
 
 from amed.dataset import get_dataset, get_loader
 from amed.models import SimSiam
-from amed.models.simsiam import get_transforms
 from amed.models.backbone import CSPDarknet
+from amed.models.simsiam import get_transforms
 
 
 def make_parser():
@@ -27,17 +27,23 @@ def make_parser():
     parser.add_argument("-e", "--epochs", default=300, type=int, help="number of total epochs to run")
     parser.add_argument("-l", "--learning-rate", default=0.05, type=float, help="initial (base) learning rate", dest="lr")
     parser.add_argument("-m", "--momentum", default=0.9, type=float, help="momentum of SGD solver")
-    parser.add_argument("-r", "--root", default="/work/hara.e/AMED/lib/dataset/cutout/images", type=str, help="please set data root")
+    parser.add_argument(
+        "-r", "--root", default="/work/hara.e/AMED/lib/dataset/cutout/images", type=str, help="please set data root"
+    )
     parser.add_argument("-s", "--seed", default=1, type=int, help="seed for initializing training")
-    parser.add_argument('-w', '--weight-decay', default=1e-4, type=float, help='weight decay (default: 1e-4)')
+    parser.add_argument("-w", "--weight-decay", default=1e-4, type=float, help="weight decay (default: 1e-4)")
 
     return parser.parse_args()
+
 
 def set_seed(seed: int = 0) -> None:
     random.seed(seed)
     torch.manual_seed(seed)
 
-def train(args: Dict, device: str, model: nn.Module, criterion, optimizer, train_loader: DataLoader, valid_loader: DataLoader) -> None:
+
+def train(
+    args: Dict, device: str, model: nn.Module, criterion, optimizer, train_loader: DataLoader, valid_loader: DataLoader
+) -> None:
     for epoch in range(args.epochs):
         model.train()
 
@@ -49,36 +55,25 @@ def train(args: Dict, device: str, model: nn.Module, criterion, optimizer, train
 def main():
     args = make_parser()
     set_seed(args.seed)
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     backbone = CSPDarknet()
     simsiam = SimSiam(backbone=backbone)
     simsiam.to(device)
     optim_params = [
-        {'params': simsiam.encoder.parameters(), 'fix_lr': False},
-        {'params': simsiam.predictor.parameters(), 'fix_lr': True}
+        {"params": simsiam.encoder.parameters(), "fix_lr": False},
+        {"params": simsiam.predictor.parameters(), "fix_lr": True},
     ]
 
     train_transform, valid_transform = get_transforms()
-    train_dataset, valid_dataset = get_dataset(
-        root=args.root,
-        train_transform=train_transform,
-        valid_transform=valid_transform
-    )
+    train_dataset, valid_dataset = get_dataset(root=args.root, train_transform=train_transform, valid_transform=valid_transform)
     train_loader, valid_loader = get_loader(
-        train_dataset=train_dataset,
-        valid_dataset=valid_dataset,
-        batch_size=args.batch_size
+        train_dataset=train_dataset, valid_dataset=valid_dataset, batch_size=args.batch_size
     )
 
     init_lr = args.lr * args.batch_size / 256
     criterion = nn.CosineSimilarity(dim=1).to(device=args.device)
-    optimizer = torch.optim.SGD(
-        optim_params,
-        init_lr,
-        momentum=args.momentum,
-        weight_decay=args.weight_decay
-    )
+    optimizer = optim.SGD(optim_params, init_lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     train(
         args=args,
@@ -87,7 +82,7 @@ def main():
         criterion=criterion,
         optimizer=optimizer,
         train_loader=train_loader,
-        valid_loader=valid_loader
+        valid_loader=valid_loader,
     )
 
 
