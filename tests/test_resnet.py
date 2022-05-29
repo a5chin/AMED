2b7bd5d2c6a36d8ffaae168687a85f9f75a57a8e
a5chin <capricorn42551230@gmail.com>
2022-05-29 23:06:12 +0900

    fixed and created tests

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -11,7 +11,9 @@ from amed.models.backbone import resnet18
 
 def test_resnet():
     imgs = torch.randn(4, 3, 32, 32)
-    model = resnet18(pretrained=True)
+    model = resnet18(num_classes=4, pretrained=True)
 
     preds = model(imgs)
     assert preds is not None
+
+    return model
