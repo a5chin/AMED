d51309af5e2df80e003720587198921c1ba94e08
a5chin <capricorn42551230@gmail.com>
2022-05-30 14:19:51 +0900

    add for saving model

--- amed/trainer.py
+++ amed/trainer.py
@@ -1,5 +1,8 @@
+from typing import Optional
 from pathlib import Path
 
+import torch
+from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
@@ -15,11 +18,12 @@ class Trainer:
         self.train_loader, self.valid_loader = train_loader, valid_loader
         self.criterion, self.optimizer, self.scheduler = criterion, optimizer, scheduler
 
-    def fit(self, model):
-        losses = AverageMeter("train_loss")
+        self.best_loss = float("inf")        
 
+    def fit(self, model: nn.Module):
         for epoch in range(self.cfg.epochs):
             model.train()
+            losses = AverageMeter("train_loss")
 
             with tqdm(self.train_loader) as pbar:
                 pbar.set_description(f"[Epoch {epoch + 1}/{self.cfg.epochs}]")
@@ -30,13 +34,13 @@ class Trainer:
 
                     loss = (self.criterion(*out0).mean() + self.criterion(*out1).mean()) / 2
                     losses.update(loss.item())
-
                     pbar.set_postfix(loss=losses.value)
+
                 self.train_writer.add_scalar(losses.avg, epoch)
 
             self.evaluate(model, epoch)
 
-    def evaluate(self, model, epoch) -> None:
+    def evaluate(self, model: nn.Module, epoch: Optional[int] = None) -> None:
         model.eval()
         losses = AverageMeter("valid_loss")
 
@@ -47,4 +51,9 @@ class Trainer:
             loss = (self.criterion(*out0).mean() + self.criterion(*out1).mean()) / 2
             losses.update(loss.item())
 
-        self.valid_writer.add_scalar(losses.avg, epoch)
+        if epoch is not None:
+            self.valid_writer.add_scalar(losses.avg, epoch)
+
+            if losses.avg <= self.best_loss:
+                self.best_acc = losses.avg
+                torch.save(model.state_dict(), Path(self.cfg.logdir) / "best.pth")
