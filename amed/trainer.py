c1007c51814d86b04b40dc0226f41306f71ad58a
a5chin <81167661+a5chin@users.noreply.github.com>
2022-06-01 03:17:13 +0900

    Create a example for Simsiam (#4)
    * create examples for reshape dataset
    
    * merged from main
    
    * fixed trainer for simsiam
    
    * create writer
    
    * adapted format
    
    * add for saving model
    
    * create logger
    
    * fixed epoch
    
    * adopted format
    
    * downgrade protobuf
    
    * fixed bugs in visualize.ipynb

--- amed/trainer.py
+++ amed/trainer.py
@@ -1,67 +1,63 @@
+from pathlib import Path
+from typing import Optional
+
 import torch
-from torch import optim
-from torch.utils.data import DataLoader
-from torchvision import transforms
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from config import config
-
-from .dataset import AMEDDataset
-from .models import YOLOX, YOLOv5
-from .models.yolov5.utils import intersect_dicts
+from amed.utils import AverageMeter, get_logger
 
 
 class Trainer:
-    def __init__(self, model_name: str = "YOLOX"):
-        self.cfg = config
-        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.model = self.pretrained_model(model_name)
-        print(self.model)
-        self.transform = {
-            "train": transforms.Compose(
-                [
-                    transforms.ToTensor(),
-                    transforms.Resize((512, 512)),
-                ]
-            ),
-            "val": transforms.Compose(
-                [
-                    transforms.ToTensor(),
-                    transforms.Resize((512, 512)),
-                ]
-            ),
-        }
-        self.optimizer = optim.SGD(self.model.parameters(), lr=1e-3)
+    def __init__(self, cfg, train_loader, valid_loader, criterion, optimizer, scheduler):
+        self.cfg = cfg
+        self.logger = get_logger()
+        self.train_writer = SummaryWriter(Path(self.cfg.logdir) / "train")
+        self.valid_writer = SummaryWriter(Path(self.cfg.logdir) / "valid")
 
-    def train(self):
-        traindataset = AMEDDataset(root=self.cfg.DATASET.ROOT, transform=self.transform["train"])
-        traindataloader = DataLoader(dataset=traindataset, batch_size=self.cfg.DATASET.BATCH_SIZE, shuffle=True, drop_last=True)
+        self.train_loader, self.valid_loader = train_loader, valid_loader
+        self.criterion, self.optimizer, self.scheduler = criterion, optimizer, scheduler
 
-        for epoch in range(self.cfg.DATASET.TOTAL_EPOCH):
-            self.model.train()
-            total, running_loss, running_acc = 0, 0.0, 0.0
+        self.best_loss = float("inf")
 
-            with tqdm(enumerate(traindataloader, 0), total=len(traindataloader)) as pbar:
-                pbar.set_description(f"[Epoch {epoch + 1}/{self.cfg.DATASET.TOTAL_EPOCH}]")
+    def fit(self, model: nn.Module):
+        for epoch in range(self.cfg.epochs):
+            model.train()
+            losses = AverageMeter("train_loss")
 
-                for _, data in pbar:
-                    images, bboxes = data
-                    images, bboxes = images.to(self.device), bboxes.to(self.device)
+            with tqdm(self.train_loader) as pbar:
+                pbar.set_description(f"[Epoch {epoch + 1}/{self.cfg.epochs}]")
 
-                    self.optimizer.zero_grad()
+                for images, _ in pbar:
+                    x0, x1 = images[0].to(self.cfg.device), images[1].to(self.cfg.device)
+                    out0, out1 = model(x0=x0, x1=x1)
 
-                    preds = self.model(images)
-                    print(preds[0].shape)
+                    loss = (self.criterion(*out0).mean() + self.criterion(*out1).mean()) / 2
+                    losses.update(loss.item())
+                    pbar.set_postfix(loss=losses.value)
 
-    def pretrained_model(self, model_name: str):
-        if "yolov5" in model_name:
-            ckpt = torch.load(f"lib/models/pretrained/{model_name}.pt")
-            model = YOLOv5(cfg=f"lib/config/{model_name}.yaml")
-            csd = ckpt["model"].float().state_dict()
-            csd = intersect_dicts(csd, model.state_dict(), exclude=["anchors"])  # intersect
-            model.load_state_dict(csd, strict=False)
-        elif "YOLOX" in model_name:
-            model = YOLOX()
-            model.load_state_dict(torch.load("lib/models/pretrained/yolox_s.pt", map_location="cpu"))
-            print("loaded!")
-        return model
+                self.train_writer.add_scalar("loss", losses.avg, epoch + 1)
+
+            self.evaluate(model, epoch)
+
+    @torch.no_grad()
+    def evaluate(self, model: nn.Module, epoch: Optional[int] = None) -> None:
+        model.eval()
+        losses = AverageMeter("valid_loss")
+
+        for images, _ in tqdm(self.valid_loader):
+            x0, x1 = images[0].to(self.cfg.device), images[1].to(self.cfg.device)
+            out0, out1 = model(x0=x0, x1=x1)
+
+            loss = (self.criterion(*out0).mean() + self.criterion(*out1).mean()) / 2
+            losses.update(loss.item())
+
+        self.logger.info(f"loss: {losses.avg}")
+
+        if epoch is not None:
+            self.valid_writer.add_scalar("loss", losses.avg, epoch + 1)
+
+            if losses.avg <= self.best_loss:
+                self.best_acc = losses.avg
+                torch.save(model.backbone.state_dict(), Path(self.cfg.logdir) / "best.pth")
