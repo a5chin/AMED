734ed2ba3584623144922c9e957ca6f7d2035931
a5chin <capricorn42551230@gmail.com>
2022-06-30 19:19:45 +0900

    fixed CenenterNet

--- amed/models/centernet/centernet.py
+++ amed/models/centernet/centernet.py
@@ -3,28 +3,38 @@ from typing import Dict
 import torch
 from torch import nn
 
+from amed.losses import GaussianFocalLoss, L1Loss
+
+from .backbone import resnet18
+from .modules import CenterNetHead, CTResNetNeck
+
 
 class CenterNet(nn.Module):
-    def __init__(
-        self,
-        num_classes: int,
-        backbone: nn.Module,
-        head: nn.Module,
-        decoder,
-        down_ratio: int,
-        embedding_dim: int,
-    ):
+    def __init__(self) -> None:
         super().__init__()
-        self.num_classes = num_classes
-        self.embedding_dim = embedding_dim
-        self.scaling_factor = down_ratio
+        self.backbone = resnet18(pretrained=True)
+        self.neck = CTResNetNeck(
+            in_channels=512,
+            num_deconv_filters=(256, 128, 64),
+            num_deconv_kernels=(4, 4, 4),
+        )
+        self.bbox_head = CenterNetHead(in_channels=64, feat_channels=64, num_classes=4)
 
-        self.backbone = backbone
-        self.head = head
-        self.decoder = decoder
+        self.loss_center_haetmap = GaussianFocalLoss(loss_weight=1.0)
+        self.loss_wh = L1Loss(loss_weight=0.1)
+        self.loss_offset = L1Loss(loss_weight=1.0)
 
     def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
-        output = self.backbone(x)
-        value = self.head(output)
+        x = self.backbone(x)
+        x = self.neck(x)
+        feature = self.bbox_head(x)
 
-        return value
+        loss_center_heatmap = self.loss_center_haetmap(feature["heatmap"])
+        loss_wh = self.loss_wh(["wh"])
+        loss_offset = self.loss_offset(["offset"])
+
+        return {
+            "los_center_heatmap": loss_center_heatmap,
+            "loss_wh": loss_wh,
+            "loss_offset": loss_offset,
+        }
