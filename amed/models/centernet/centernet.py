0f4f2ce6935023df0fc7244756ef4f72b6027e1e
a5chin <capricorn42551230@gmail.com>
2022-06-30 19:39:22 +0900

    apply format

--- amed/models/centernet/centernet.py
+++ amed/models/centernet/centernet.py
@@ -3,8 +3,6 @@ from typing import Dict
 import torch
 from torch import nn
 
-from amed.losses import GaussianFocalLoss, L1Loss
-
 from .backbone import resnet18
 from .modules import CenterNetHead, CTResNetNeck
 
@@ -20,21 +18,9 @@ class CenterNet(nn.Module):
         )
         self.bbox_head = CenterNetHead(in_channels=64, feat_channels=64, num_classes=4)
 
-        self.loss_center_haetmap = GaussianFocalLoss(loss_weight=1.0)
-        self.loss_wh = L1Loss(loss_weight=0.1)
-        self.loss_offset = L1Loss(loss_weight=1.0)
-
     def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
         x = self.backbone(x)
         x = self.neck(x)
         feature = self.bbox_head(x)
 
-        loss_center_heatmap = self.loss_center_haetmap(feature["heatmap"])
-        loss_wh = self.loss_wh(["wh"])
-        loss_offset = self.loss_offset(["offset"])
-
-        return {
-            "los_center_heatmap": loss_center_heatmap,
-            "loss_wh": loss_wh,
-            "loss_offset": loss_offset,
-        }
+        return feature
