fc02efa1d6797439a2fb8ade7d22e878db7c9a17
a5chin <capricorn42551230@gmail.com>
2022-07-07 02:50:45 +0900

    create losses

--- tests/test_centernet.py
+++ tests/test_centernet.py
@@ -1,19 +1,20 @@
 import sys
-import warnings
 from pathlib import Path
 
 import torch
 
 current_dir = Path(__file__).resolve().parent
-sys.path.append(str(current_dir) + "/../")
+sys.path.append(str(current_dir.parent))
 
 from amed.models.centernet import CenterNet
 
 
 def test_model():
-    images = torch.randn(4, 3, 255, 255)
+    images = torch.randn(16, 3, 512, 512)
     model = CenterNet()
 
     out = model(images)
 
-    assert isinstance(out, dict)
+    assert out["heatmap"].shape == (16, 4, 128, 128)
+    assert out["wh"].shape == (16, 2, 128, 128)
+    assert out["offset"].shape == (16, 2, 128, 128)
